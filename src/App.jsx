13ea90b5213c91c7c5e73ef842ac3d00e05f83a3
Artyom Smirnov <artyomsmirnov1111@gmail.com>
2024-04-08 12:37:40 +0300

    add home ,login and signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
+import Login from "./pages/login/Login";
 
 function App() {
-
   return (
-    <>
-      <button className="btn btn-primary">Primary</button>
-    </>
-  )
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Login />
+    </div>
+  );
 }
 
-export default App
+export default App;
