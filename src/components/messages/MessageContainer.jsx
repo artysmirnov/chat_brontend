8cb1a5c5dd60cbd69818ee695baf5a955c33199b
Artyom Smirnov <artyomsmirnov1111@gmail.com>
2024-04-10 22:15:57 +0300

    add NoChatSelected

--- src/components/messages/MessageContainer.jsx
+++ src/components/messages/MessageContainer.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import Messages from "./Messages"
-import MessageInput from './MessageInput'
-const MessageContainer = () => {
-  return (
-    <div className='md:min-w-[450px] flex flex-col'>
-      <>
-        <div className='bg-slate-500 px-4 py-2 mb-2'>
-            <span className='label-text'>To: </span> <span className='text-gray-900 font-bold'>User Name</span>
-        </div>
-      </>
-      <Messages/>
-      <MessageInput/>
-    </div>
-  )
-}
+import Messages from "./Messages";
+import MessageInput from "./MessageInput";
+import NoChatSelected from "./NoChatSelected";
 
-export default MessageContainer
+const MessageContainer = () => {
+  const noChatSelected = true;
+  return (
+    <div className="md:min-w-[450px] flex flex-col">
+      {noChatSelected ? (
+        <NoChatSelected />
+      ) : (
+        <>
+          <div className="bg-slate-500 px-4 py-2 mb-2">
+            <span className="label-text">To: </span>{" "}
+            <span className="text-gray-900 font-bold">User Name</span>
+          </div>
+          <Messages />
+          <MessageInput />{" "}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default MessageContainer;
