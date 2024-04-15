5c1365cdcbdf75c9d41bcc68797c74d6f06b5384
Artyom Smirnov <artyomsmirnov1111@gmail.com>
2024-04-15 14:10:50 +0300

    add getMessages functionality

--- src/components/messages/Message.jsx
+++ src/components/messages/Message.jsx
@@ -1,20 +1,28 @@
-import React from "react";
+import { useAuthContext } from "../../context/AuthContext"
+import useConversation from "../../zustand/useConversation"
 
-const Message = () => {
-  return (
-    <div className="chat chat-end">
-      <div className="chat-image avatar">
-        <div className="w-10 rounded-full">
-          <img
-            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-            alt="user avatar"
-          />
-        </div>
-      </div>
-      <div className="chat-bubble text-white bg-blue-500">Hi whats up</div>
-    <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">11:42</div>
-    </div>
-  );
-};
+const Message = ({ message }) => {
+	const { authUser } = useAuthContext()
+	const { selectedConversation } = useConversation()
+	const fromMe = message.senderId === authUser._id
+	const chatClassName = fromMe ? "chat-end" : "shat-start"
+	const profilePic = fromMe
+		? authUser.profilePic
+		: selectedConversation?.profilePic
+	const bubbleBgColor = fromMe ? "bg-blue-500 " : ""
+	return (
+		<div className={`chat ${chatClassName}`}>
+			<div className="chat-image avatar">
+				<div className="w-10 rounded-full">
+					<img src={profilePic} alt="user avatar" />
+				</div>
+			</div>
+			<div className={`chat-bubble ${bubbleBgColor}`}>{message.message}</div>
+			<div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
+				11:42
+			</div>
+		</div>
+	)
+}
 
-export default Message;
+export default Message
--- src/components/messages/Messages.jsx
+++ src/components/messages/Messages.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import Message from './Message'
+import useGetMessages from "../../hooks/useGetMessages"
+import Message from "./Message"
 
 const Messages = () => {
-  return (
-    <div className='px-4 flex-1 overflow-auto'>
-    
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-    </div>
-  )
+	const { messages, loading } = useGetMessages()
+	return (
+		<div className="px-4 flex-1 overflow-auto">
+			{!loading &&
+				messages.length > 0 &&
+				messages.map(message => (
+					<div key={message._id}>
+						<Message message={message} />
+					</div>
+				))}
+		</div>
+	)
 }
 
 export default Messages
