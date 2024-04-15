6e7a2e3b4995cca3152c19b4d2a1a46875ea462b
Artyom Smirnov <artyomsmirnov1111@gmail.com>
2024-04-15 15:07:36 +0300

    add sockets and online status

--- src/components/messages/NoChatSelected.jsx
+++ src/components/messages/NoChatSelected.jsx
@@ -1,14 +1,16 @@
-import { TiMessage } from "react-icons/ti";
+import { TiMessage } from "react-icons/ti"
+import { useAuthContext } from "../../context/AuthContext"
 
 const NoChatSelected = () => {
-    return (
-      <div className="flex items-center justify-center w-full h-full">
-        <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-          <p>Welcome</p>
-          <p>Select chat</p>
-          <TiMessage className="text-3xl md:text-6xl text-center"></TiMessage>
-        </div>
-      </div>
-    );
-  };
-  export default NoChatSelected
+	const { authUser } = useAuthContext()
+	return (
+		<div className="flex items-center justify-center w-full h-full">
+			<div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
+				<p>Welcom {authUser.fullName}</p>
+				<p>Select chat</p>
+				<TiMessage className="text-3xl md:text-6xl text-center"></TiMessage>
+			</div>
+		</div>
+	)
+}
+export default NoChatSelected
